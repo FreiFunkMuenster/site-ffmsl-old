49f19efe9899b3b20682d70fee161c451437b9e4
Christian 'VOID' Elberfeld <elberfeld@web.de>
2014-05-13 01:19:08 +0200

    modules datei und batman-wan modul

--- site.mk
+++ site.mk
@@ -1,12 +1,14 @@
 GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-autoupdater \
+	gluon-batman-on-wan \
 	gluon-config-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
 	gluon-next-node \
 	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
