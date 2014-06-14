bba4bb6c4b5e4b90ffb9c034eee149598857db94
Christian 'VOID' Elberfeld <elberfeld@web.de>
2014-06-14 22:49:47 +0200

    batman on wan und portconfig wieder mit eingebaut

--- site.mk
+++ site.mk
@@ -1,12 +1,14 @@
 GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-autoupdater \
+	gluon-batman-on-wan \
 	gluon-config-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
 	gluon-next-node \
 	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
@@ -15,4 +17,3 @@ GLUON_SITE_PACKAGES := \
 	iwinfo \
 	iptables \
 	haveged
-
