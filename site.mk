38057de4c8a0a30bf88163e34aa85f2e521a507b
Jan-Marten Brüggemann <fussel@fusselkater.org>
2014-08-04 21:53:21 +0200

    Anpassung MTU für v6, Anpassung gluon-pakete für 2014.3

--- site.mk
+++ site.mk
@@ -1,17 +1,20 @@
 GLUON_SITE_PACKAGES := \
 	gluon-alfred \
+	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
-	gluon-next-node \
+	gluon-luci-portconfig \
 	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
+	gluon-next-node \
 	gluon-radvd \
 	gluon-status-page \
 	iwinfo \
 	iptables \
 	haveged
+
+GLUON_PRIORITY := 3
