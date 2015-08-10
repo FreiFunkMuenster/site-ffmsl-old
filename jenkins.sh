e9d55c31ef0027eb3c9e98a66d6b48c907e33b35
Matthias P. Walther <matthiaspeterw@aol.com>
2015-08-10 14:12:02 +0200

    - Gluon-Version für Jenkins auf v2015.1.2 erhöht

--- jenkins.sh
+++ jenkins.sh
@@ -17,7 +17,7 @@
 ###############################################################################################
 
 # Globale Einstellungen 
-GLUON_COMMIT=v2015.1.1
+GLUON_COMMIT=v2015.1.2
 
 # Bei Ausführung auf dem Buildserver ist die Variable $WORKSPACE gesetzt 
 # andernfalls wird das aktuelle Verzeichnis verwendet  
