2ca6e263a22161367668bb9c601187b4cfe8d486
Matthias P. Walther <matthiaspeterw@aol.com>
2015-11-18 10:48:59 +0100

    - site.mk alphabetisch sortiert  - site.conf fastd repariert

--- site.mk
+++ site.mk
@@ -6,9 +6,9 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
-	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
@@ -16,12 +16,12 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
 	gluon-luci-wifi-config \
-	gluon-next-node \
 	gluon-mesh-batman-adv-15 \
 	gluon-mesh-vpn-fastd \
+	gluon-next-node \
 	gluon-radvd \
-	gluon-status-page \
 	gluon-setup-mode \
+	gluon-status-page \
 	haveged \
 	iptables \
 	iwinfo
