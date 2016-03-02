ed9f3dec84ec4d3545cc9af11847820c5d3e016d
SilversurferCH <Kev.Ut@gmx.de>
2016-03-02 20:59:51 +0100

    Update build.sh
    Ablage in Ordner implementiert

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@
 # Das Script benötigt die folgenden Kommandozeilenparameter:
 # - Gluon-Commit (z.B. v2014.4)
 # - Build-Nummer (z.B. 114)
+# - Ordner in dem abgelegt werden soll (z.B. domaene01) 
 # - Optionale Parameter für make (z.B. V=s oder -j 4)
 #  
 ###############################################################################################
@@ -28,20 +29,22 @@ if [ "x$WORKSPACE" = "x" ]; then
 	WORKSPACE=`pwd`
 fi
 
-# Build Verzeichnis vorbereiten 
+# Build Verzeichnis vorbereiten
+mkdir /var/www/html/$3/versions/v$2
+letzterBefehlErfolgreich;
 cd $WORKSPACE
 sh ./prepare.sh $1
 letzterBefehlErfolgreich;
 
 # Gluon Pakete aktualisieren und Build ausführen 
 cd $WORKSPACE
-sh ./compile.sh $1 $2 ar71xx-generic $4 $5 $6 $7 $8 $9
+sh ./compile.sh $1 $2 ar71xx-generic GLUON_IMAGEDIR=/var/www/html/$3/versions/v$2 $4 $5 $6 $7 $8 $9
 letzterBefehlErfolgreich;
-sh ./compile.sh $1 $2 ar71xx-nand $4 $5 $6 $7 $8 $9
+sh ./compile.sh $1 $2 ar71xx-nand GLUON_IMAGEDIR=/var/www/html/$3/versions/v$2 $4 $5 $6 $7 $8 $9
 letzterBefehlErfolgreich;
-sh ./compile.sh $1 $2 mpc85xx-generic $4 $5 $6 $7 $8 $9
+sh ./compile.sh $1 $2 mpc85xx-generic GLUON_IMAGEDIR=/var/www/html/$3/versions/v$2 $4 $5 $6 $7 $8 $9 
 letzterBefehlErfolgreich;
-sh ./compile.sh $1 $2 x86-generic $4 $5 $6 $7 $8 $9
+sh ./compile.sh $1 $2 x86-generic GLUON_IMAGEDIR=/var/www/html/$3/versions/v$2 $4 $5 $6 $7 $8 $9 
 letzterBefehlErfolgreich;
-sh ./compile.sh $1 $2 x86-kvm_guest $4 $5 $6 $7 $8 $9
+sh ./compile.sh $1 $2 x86-kvm_guest GLUON_IMAGEDIR=/var/www/html/$3/versions/v$2 $4 $5 $6 $7 $8 $9 
 letzterBefehlErfolgreich;
