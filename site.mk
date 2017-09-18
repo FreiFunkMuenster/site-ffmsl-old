d0fb47905d018180d6b1449489cb58082a1cd65e
HellMar <github@hellmann.me>
2017-09-18 08:56:47 +0200

    Revert "Update site.mk"
    This reverts commit ed7ff85168a962b1ae1d68ffc54bd37b1826e138.

--- site.mk
+++ site.mk
@@ -7,26 +7,28 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
+	gluon-config-mode-tunneldigger \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-network \
-	gluon-web-private-wifi \
-	gluon-web-wifi-config \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
 	gluon-mesh-batman-adv-15 \
 	gluon-mesh-vpn-tunneldigger \
+	gluon-next-node \
 	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
 	haveged \
 	iptables \
-	gluon-mesh-vpn-core \
+	gluon-tunneldigger-watchdog \
+	gluon-migrate-vpn \
 	iwinfo \
 	gluon-ssid-changer \
 	gluon-config-mode-show-mac \
-	gluon-rfkill-disable \
-	gluon-authorized-keys
+	gluon-rfkill-disable
 
 GLUON_PRIORITY ?= 3
 GLUON_LANGS ?= en de
