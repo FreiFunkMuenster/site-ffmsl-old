d681010e51998d8627caf8c7c90d4a0b2f6ec044
Matthias P. Walther <matthiaspeterw@aol.com>
2017-12-31 14:29:07 +0100

    - site.mk repariert

--- site.mk
+++ site.mk
@@ -1,6 +1,4 @@
 GLUON_SITE_PACKAGES := \
-	ffpb-ebtables-net-rules \
-	gluon-alfred \
 	gluon-respondd\
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
@@ -8,29 +6,26 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
-	gluon-config-mode-tunneldigger \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
+	gluon-ebtables-source-filter \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-private-wifi \
+	gluon-web-wifi-config \
 	gluon-mesh-batman-adv-15 \
 	gluon-mesh-vpn-tunneldigger \
-	gluon-next-node \
 	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
 	haveged \
 	iptables \
-	gluon-tunneldigger-watchdog \
-	gluon-migrate-vpn \
+	gluon-mesh-vpn-core \
 	iwinfo \
 	gluon-ssid-changer \
 	gluon-config-mode-show-mac \
 	gluon-rfkill-disable
-
 
 GLUON_PRIORITY ?= 3
 GLUON_LANGS ?= en de
