4cd618a7aa704a67d9d4bed239f38681d138fb6e
RobWei <admin@robwei.me>
2019-01-10 20:27:10 +0100

    * disabled unimportant packages

--- site.mk
+++ site.mk
@@ -1,6 +1,4 @@
 GLUON_SITE_PACKAGES := \
-	ffpb-ebtables-net-rules \
-	gluon-alfred \
 	gluon-respondd\
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
@@ -8,24 +6,28 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
+	gluon-ebtables-source-filter \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-private-wifi \
+	gluon-web-wifi-config \
 	gluon-mesh-batman-adv-15 \
-	gluon-mesh-vpn-fastd \
-	gluon-next-node \
-	uradvd \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-radvd \
 	gluon-setup-mode \
 	gluon-status-page \
 	haveged \
 	iptables \
-	iwinfo
-	 
+	gluon-mesh-vpn-core \
+	iwinfo #\
+	#gluon-ssid-changer \
+	#gluon-config-mode-show-mac \
+	#gluon-rfkill-disable
 
 GLUON_PRIORITY ?= 3
 GLUON_LANGS ?= en de
+GLUON_ATH10K_MESH := ibss
+GLUON_REGION := eu
